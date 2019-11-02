876d5e2bf5f0765e5b907af1ef8f802639d992a5
coalman321 <coalman321@users.noreply.github.com>
2019-11-02 19:41:36 -0400

    fix unreachable catch

--- app/src/main/java/com/example/pathy/MappingController.java
+++ app/src/main/java/com/example/pathy/MappingController.java
@@ -36,8 +36,6 @@ public class MappingController{
         }
         catch (FileNotFoundException e) {
             Log.e("login activity", "File not found: " + e.toString());
-        } catch (IOException e) {
-            Log.e("login activity", "Can not read file: " + e.toString());
         }
 
     }
