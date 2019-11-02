40fc3529ecb5d1f87e575fd8c8deb33b21a1e9ce
Eddyl112 <edwardlui17@gmail.com>
2019-11-02 18:25:03 -0400

    Worked on searchbar
    Added suggestions and stuff

--- app/src/main/java/com/example/pathy/MainActivity.java
+++ app/src/main/java/com/example/pathy/MainActivity.java
@@ -1,25 +1,28 @@
 package com.example.pathy;
 
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.SearchView;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  {
     public SearchView userSearch;
     public SearchBar userSearchClass;
+    public LinearLayout suggestion;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         userSearch = findViewById(R.id.search);
+        suggestion = findViewById(R.id.linearLayout);
         userSearch.setSubmitButtonEnabled(true);
-        userSearchClass.registerSearchListeners(userSearch, getApplicationContext());
-
-
+        userSearch.setQueryHint("Type a room...");
+        userSearch.setIconifiedByDefault(false);
+        userSearchClass.registerSearchListeners(userSearch, suggestion, getApplicationContext());
     }
 }
--- app/src/main/java/com/example/pathy/SearchBar.java
+++ app/src/main/java/com/example/pathy/SearchBar.java
@@ -1,17 +1,38 @@
 package com.example.pathy;
 
+
+import android.app.ListActivity;
 import android.content.Context;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.SearchView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.content.SearchRecentSuggestionsProvider;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchBar {
 
+    public SearchBar(Context context) {
+
+    }
+
 
     public static String userInput;
+    public static final String[] SUGGESTLIST = {"Archie M. Griffin Grand Ballroom",
+            "Senate Chambers", "Student-Alumni Council Room", "Sphinx Centennial Leadership Suite",
+            "Ohio Staters, Inc. Founders Room", "Ohio Staters, Inc. Traditions Room",
+            "Glass Art Lounge", "Keith B. Key Center for Student Leadership and Service",
+            "Administrative Office Suite", "Danny Price Student Lounge"};
+    private SimpleCursorAdapter adapter;
 
-
-    public static void registerSearchListeners(SearchView userSearch, final Context con) {
+    public static void registerSearchListeners(SearchView userSearch, final LinearLayout suggestion, final Context con) {
         userSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -24,12 +45,26 @@ public class SearchBar {
             public boolean onQueryTextChange(String newText) {
                 Toast.makeText(con, "Input: " + newText, Toast.LENGTH_LONG).show();
                 Log.e("Text change", "Input: " + newText);
+                dropDownList(newText, suggestion, con);
                 return false;
             }
         });
+
+
     }
 
-    public static void dropDownList() {
+    public static void dropDownList(String text, LinearLayout suggestion, Context context) {
+        suggestion.removeAllViews();
+        int length = text.length();
+        List<String> suggested = new ArrayList<String>();
+        for (int i = 0; i < SUGGESTLIST.length; i++) {
+            if (text.compareToIgnoreCase(SUGGESTLIST[i].substring(0, length)) == 0) {
+                TextView textView = new TextView(context);
+                textView.setText(SUGGESTLIST[i]);
+                suggestion.addView(textView);
+            }
+        }
+
 
     }
 
