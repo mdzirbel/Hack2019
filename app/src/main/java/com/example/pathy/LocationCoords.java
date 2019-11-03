90cdc2612e93c9e7d453de5c2c61e73dd9f8483e
Maxwell <maxfojtik@bex.net>
2019-11-03 09:08:21 -0500

    make it so when you move it invalidates the view

--- app/src/main/java/com/example/pathy/LocationCoords.java
+++ app/src/main/java/com/example/pathy/LocationCoords.java
@@ -15,9 +15,9 @@ import com.example.pathy.aStar.Node;
 public class LocationCoords implements LocationListener {
 
     Node currentNode = new Node(40, 51); // The node you're currently at
-
+    MapPanning mp;
     public LocationCoords(Context context, MainActivity that) {
-
+        this.mp = that.findViewById(R.id.im_move_zoom_rotate);
         if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(that, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
         }
@@ -35,6 +35,7 @@ public class LocationCoords implements LocationListener {
         if (MappingController.isHasInit()) {
             currentNode = MappingController.coordToNode(lon, lat);
             MapPanning.currentNode = currentNode;
+            mp.postInvalidate();
         }
 
         Log.d("LOCATION", "Lon: " + lon + ", Lat: " + lat);
