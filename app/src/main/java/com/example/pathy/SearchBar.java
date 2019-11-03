cdd6993686f470c1097f9bf82e4c13d34592d079
Matthew <matthew.d.zirbel@gmail.com>
2019-11-03 00:33:44 -0400

    Added functionality to query buttons

--- app/src/main/java/com/example/pathy/SearchBar.java
+++ app/src/main/java/com/example/pathy/SearchBar.java
@@ -18,30 +18,32 @@ import java.util.List;
 
 public class SearchBar {
 
-    public SearchBar(Context context) {
+    SearchView search;
 
+    public SearchBar(SearchView searchView) {
+        search = searchView;
     }
 
 
-    public static String userInput;
-    public static final String[] SUGGESTLIST = {"Archie M. Griffin Grand Ballroom",
+    private static final String[] SUGGESTLIST = {"Archie M. Griffin Grand Ballroom",
             "Senate Chambers", "Student-Alumni Council Room", "Sphinx Centennial Leadership Suite",
             "Ohio Staters, Inc. Founders Room", "Ohio Staters, Inc. Traditions Room",
             "Glass Art Lounge", "Keith B. Key Center for Student Leadership and Service",
             "Administrative Office Suite", "Danny Price Student Lounge"};
     private SimpleCursorAdapter adapter;
 
-    public static void registerSearchListeners(final SearchView userSearch, final LinearLayout suggestion, final Context con) {
+    void registerSearchListeners(final SearchView userSearch, final LinearLayout suggestion, final Context con) {
         userSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                userInput = query;
-                Log.e("Submit!", "Input: " + query);
+                Log.d("QUERY SUBMIT", "Input: " + query);
+                submitQuery(query);
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
+
                 Toast.makeText(con, "Input: " + newText, Toast.LENGTH_LONG).show();
                 Log.e("Text change", "Input: " + newText);
 
@@ -50,29 +52,27 @@ public class SearchBar {
                 return false;
             }
         });
-
-
     }
 
-    public static void dropDownList(String text, LinearLayout suggestion, Context context, SearchView userSearch) {
+    private void dropDownList(String text, LinearLayout suggestion, Context context, SearchView userSearch) {
         Arrays.sort(SUGGESTLIST);
         suggestion.removeAllViews();
         int length = text.length();
-        List<String> suggested = new ArrayList<String>();
+        List<String> suggested = new ArrayList<>();
         suggestion.getLayoutParams().height = 0;
-
 
         if (text.length() == 0) {
             suggestion.getLayoutParams().height = 0;
             suggestion.removeAllViews();
         } else {
             suggestion.getLayoutParams().height = 600;
-            for (int i = 0; i < SUGGESTLIST.length; i++) {
-                if (text.compareToIgnoreCase(SUGGESTLIST[i].substring(0, length)) == 0) {
+            for (String s : SUGGESTLIST) {
+                if (text.compareToIgnoreCase(s.substring(0, length)) == 0) {
                     Button button = new Button(context);
-                    button.setText(SUGGESTLIST[i]);
+                    button.setText(s);
                     button.setBackgroundResource(R.drawable.border);
                     button.setBackgroundColor(Color.TRANSPARENT);
+                    button.setOnClickListener(onSuggestionClickListener);
                     suggestion.addView(button);
                 }
             }
@@ -82,4 +82,25 @@ public class SearchBar {
 
     }
 
+    // For clicking the suggestions
+    private View.OnClickListener onSuggestionClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            // Get the text of the button you selected
+            String buttonText = (String) ((Button) v).getText();
+
+            // Fill in the whole text in the search box and send the query
+            search.setQuery(buttonText, true);
+        }
+    };
+
+    private static void submitQuery(String query) {
+
+
+
+        Log.d("SUMBIT QUERY", query);
+    }
+
+getRoomNodes(name)
 }
